6cf0a4abf28b2b23e9fa52475f880017e95abb4b
ChoiPuReum <pooreumsunny@gmail.com>
2021-12-13 23:01:45 +0900

    fix typo
    Signed-off-by: ChoiPuReum <pooreumsunny@gmail.com>

--- dist/code/MarkdownParser.js
+++ dist/code/MarkdownParser.js
@@ -1,19 +1,19 @@
+"use strict";
 //#region TagType definitions
-
-enum TagType {
-    Paragraph,
-    Header1,
-    Header2,
-    Header3,
-    Header4,
-    Header5,
-    Header6,
-    HorizontalRule
-}
-
+var TagType;
+(function (TagType) {
+    TagType[TagType["Paragraph"] = 0] = "Paragraph";
+    TagType[TagType["Header1"] = 1] = "Header1";
+    TagType[TagType["Header2"] = 2] = "Header2";
+    TagType[TagType["Header3"] = 3] = "Header3";
+    TagType[TagType["Header4"] = 4] = "Header4";
+    TagType[TagType["Header5"] = 5] = "Header5";
+    TagType[TagType["Header6"] = 6] = "Header6";
+    TagType[TagType["HorizontalRule"] = 7] = "HorizontalRule";
+})(TagType || (TagType = {}));
 class TagTypeToHtml {
-    private readonly tagType : Map<TagType, string> = new Map<TagType, string>();
     constructor() {
+        this.tagType = new Map();
         this.tagType.set(TagType.Header1, "h1");
         this.tagType.set(TagType.Header2, "h2");
         this.tagType.set(TagType.Header3, "h3");
@@ -21,18 +21,15 @@ class TagTypeToHtml {
         this.tagType.set(TagType.Header5, "h5");
         this.tagType.set(TagType.Header6, "h6");
         this.tagType.set(TagType.Paragraph, "p");
-        this.tagType.set(TagType.HorizontalRule, "hr")
+        this.tagType.set(TagType.HorizontalRule, "hr");
     }
-
-    public OpeningTag(tagType : TagType) : string {
+    OpeningTag(tagType) {
         return this.GetTag(tagType, `<`);
     }
-
-    public ClosingTag(tagType : TagType) : string {
+    ClosingTag(tagType) {
         return this.GetTag(tagType, `</`);
     }
-
-    private GetTag(tagType : TagType, openingTagPattern : string) : string {
+    GetTag(tagType, openingTagPattern) {
         let tag = this.tagType.get(tagType);
         if (tag !== null) {
             return `${openingTagPattern}${tag}>`;
@@ -40,32 +37,24 @@ class TagTypeToHtml {
         return `${openingTagPattern}p>`;
     }
 }
-
-//#endregion TagType definitions
-
-//#region Markdown document
-interface IMarkdownDocument {
-    Add(...content : string[]) : void;
-    Get() : string;
-}
-
-class MarkdownDocument implements IMarkdownDocument {
-    private content : string = "";
-    Add(...content: string[]): void {
+class MarkdownDocument {
+    constructor() {
+        this.content = "";
+    }
+    Add(...content) {
         content.forEach(element => {
             this.content += element;
         });
-    }    
-    Get(): string {
+    }
+    Get() {
         return this.content;
     }
 }
 //#endregion Markdown document
-
 //#region Parsing elements
 class LineParser {
-    public Parse(value : string, tag : string) : [boolean, string] {
-        let output : [boolean, string] = [false, ""];
+    Parse(value, tag) {
+        let output = [false, ""];
         output[1] = value;
         if (value === "") {
             return output;
@@ -78,44 +67,35 @@ class LineParser {
         return output;
     }
 }
-
 class ParseElement {
-    CurrentLine : string = "";
+    constructor() {
+        this.CurrentLine = "";
+    }
 }
-
-//#endregion Parsing elements
-
-//#region Visitor pattern base
-interface IVisitor {
-    Visit(token : ParseElement, markdownDocument : IMarkdownDocument) : void;
-}
-
-interface IVisitable {
-    Accept(visitor : IVisitor, token : ParseElement, markdownDocument : IMarkdownDocument) : void;
-}
-
-class Visitable implements IVisitable {
-    Accept(visitor: IVisitor, token: ParseElement, markdownDocument: IMarkdownDocument): void {
+class Visitable {
+    Accept(visitor, token, markdownDocument) {
         visitor.Visit(token, markdownDocument);
     }
 }
-
-abstract class VisitorBase implements IVisitor {
-    constructor (private readonly tagType : TagType, private readonly TagTypeToHtml : TagTypeToHtml) {}
-    Visit(token: ParseElement, markdownDocument: IMarkdownDocument): void {
+class VisitorBase {
+    constructor(tagType, TagTypeToHtml) {
+        this.tagType = tagType;
+        this.TagTypeToHtml = TagTypeToHtml;
+    }
+    Visit(token, markdownDocument) {
         markdownDocument.Add(this.TagTypeToHtml.OpeningTag(this.tagType), token.CurrentLine, this.TagTypeToHtml.ClosingTag(this.tagType));
     }
 }
 //#endregion Visitor pattern base
-
 //#region Chain of responsibility implementation
-abstract class Handler<T> {
-    protected next : Handler<T> | null = null;
-    public SetNext(next : Handler<T>) : void {
+class Handler {
+    constructor() {
+        this.next = null;
+    }
+    SetNext(next) {
         this.next = next;
     }
-
-    public HandleRequest(request : T) : void {
+    HandleRequest(request) {
         if (!this.CanHandle(request)) {
             if (this.next != null) {
                 this.next.HandleRequest(request);
@@ -123,146 +103,126 @@ abstract class Handler<T> {
             return;
         }
     }
-
-    protected abstract CanHandle(request : T) : boolean;
 }
-
-class ParseChainHandler extends Handler<ParseElement> {
-    private readonly visitable : IVisitable = new Visitable();
-    protected CanHandle(request: ParseElement): boolean {
+class ParseChainHandler extends Handler {
+    constructor(document, tagType, visitor) {
+        super();
+        this.document = document;
+        this.tagType = tagType;
+        this.visitor = visitor;
+        this.visitable = new Visitable();
+    }
+    CanHandle(request) {
         let split = new LineParser().Parse(request.CurrentLine, this.tagType);
-        if (split[0]){
+        if (split[0]) {
             request.CurrentLine = split[1];
             this.visitable.Accept(this.visitor, request, this.document);
         }
         return split[0];
     }
-    constructor(private readonly document : IMarkdownDocument, 
-        private readonly tagType : string, 
-        private readonly visitor : IVisitor) {
-        super();
-    }
 }
 //#endregion Chain of responsibility implementation
-
 //#region Concrete visitor
 class Header1Visitor extends VisitorBase {
     constructor() {
         super(TagType.Header1, new TagTypeToHtml());
     }
 }
-
 class Header2Visitor extends VisitorBase {
     constructor() {
         super(TagType.Header2, new TagTypeToHtml());
     }
 }
-
 class Header3Visitor extends VisitorBase {
     constructor() {
         super(TagType.Header3, new TagTypeToHtml());
     }
 }
-
 class Header4Visitor extends VisitorBase {
     constructor() {
         super(TagType.Header4, new TagTypeToHtml());
     }
 }
-
 class Header5Visitor extends VisitorBase {
     constructor() {
         super(TagType.Header5, new TagTypeToHtml());
     }
 }
-
 class Header6Visitor extends VisitorBase {
     constructor() {
         super(TagType.Header6, new TagTypeToHtml());
     }
 }
-
 class ParagraphVisitor extends VisitorBase {
     constructor() {
         super(TagType.Paragraph, new TagTypeToHtml());
     }
 }
-
 class HorizontalRuleVisitor extends VisitorBase {
     constructor() {
         super(TagType.HorizontalRule, new TagTypeToHtml());
     }
 }
 //#endregion
-
 //#region Concrete chain of responsibility
 class Header1ChainHandler extends ParseChainHandler {
-    constructor(document : IMarkdownDocument) {
+    constructor(document) {
         super(document, "# ", new Header1Visitor());
     }
 }
-
 class Header2ChainHandler extends ParseChainHandler {
-    constructor(document : IMarkdownDocument) {
+    constructor(document) {
         super(document, "## ", new Header2Visitor());
     }
 }
-
 class Header3ChainHandler extends ParseChainHandler {
-    constructor(document : IMarkdownDocument) {
+    constructor(document) {
         super(document, "### ", new Header3Visitor());
     }
 }
-
 class Header4ChainHandler extends ParseChainHandler {
-    constructor(document : IMarkdownDocument) {
+    constructor(document) {
         super(document, "#### ", new Header4Visitor());
     }
 }
-
 class Header5ChainHandler extends ParseChainHandler {
-    constructor(document : IMarkdownDocument) {
+    constructor(document) {
         super(document, "##### ", new Header5Visitor());
     }
 }
-
 class Header6ChainHandler extends ParseChainHandler {
-    constructor(document : IMarkdownDocument) {
+    constructor(document) {
         super(document, "###### ", new Header6Visitor());
     }
 }
-
 class HorizontalRuleHandler extends ParseChainHandler {
-    constructor(document : IMarkdownDocument) {
+    constructor(document) {
         super(document, "---", new HorizontalRuleVisitor());
     }
 }
-
-class ParagraphHandler extends Handler<ParseElement> {
-    private readonly visitable : IVisitable = new Visitable();
-    private readonly visitor : IVisitor = new ParagraphVisitor()
-    protected CanHandle(request: ParseElement): boolean {
+class ParagraphHandler extends Handler {
+    constructor(document) {
+        super();
+        this.document = document;
+        this.visitable = new Visitable();
+        this.visitor = new ParagraphVisitor();
+    }
+    CanHandle(request) {
         this.visitable.Accept(this.visitor, request, this.document);
         return true;
     }
-    constructor(private readonly document : IMarkdownDocument) {
-        super();
-    }
 }
-
 //#endregion
-
 class ChainOfResponsibilityFactory {
-    Build(document : IMarkdownDocument) : ParseChainHandler {
-        let header1 : Header1ChainHandler = new Header1ChainHandler(document);
-        let header2 : Header2ChainHandler = new Header2ChainHandler(document);
-        let header3 : Header3ChainHandler = new Header3ChainHandler(document);
-        let header4 : Header4ChainHandler = new Header4ChainHandler(document);
-        let header5 : Header5ChainHandler = new Header5ChainHandler(document);
-        let header6 : Header6ChainHandler = new Header6ChainHandler(document);
-        let horizontalRule : HorizontalRuleHandler = new HorizontalRuleHandler(document);
-        let paragraph : ParagraphHandler = new ParagraphHandler(document);
-
+    Build(document) {
+        let header1 = new Header1ChainHandler(document);
+        let header2 = new Header2ChainHandler(document);
+        let header3 = new Header3ChainHandler(document);
+        let header4 = new Header4ChainHandler(document);
+        let header5 = new Header5ChainHandler(document);
+        let header6 = new Header6ChainHandler(document);
+        let horizontalRule = new HorizontalRuleHandler(document);
+        let paragraph = new ParagraphHandler(document);
         header1.SetNext(header2);
         header2.SetNext(header3);
         header3.SetNext(header4);
@@ -270,43 +230,39 @@ class ChainOfResponsibilityFactory {
         header5.SetNext(header6);
         header6.SetNext(horizontalRule);
         horizontalRule.SetNext(paragraph);
-
         return header1;
     }
 }
-
 class Markdown {
-    public ToHtml(text : string) : string {
-        let document : IMarkdownDocument = new MarkdownDocument();
-        let header1 : Header1ChainHandler = new ChainOfResponsibilityFactory().Build(document);
-
-        let lines : string[] = text.split(`\n`);
+    ToHtml(text) {
+        let document = new MarkdownDocument();
+        let header1 = new ChainOfResponsibilityFactory().Build(document);
+        let lines = text.split(`\n`);
         for (let index = 0; index < lines.length; index++) {
-            let parseElement : ParseElement = new ParseElement();
+            let parseElement = new ParseElement();
             parseElement.CurrentLine = lines[index];
             header1.HandleRequest(parseElement);
         }
         return document.Get();
     }
 }
-
 class HtmlHandler {
-    private markdownChange : Markdown = new Markdown;
-    public TextChangeHandler(id : string, output : string) : void {
-        let markdown = <HTMLTextAreaElement>document.getElementById(id);
-        let markdownOutput = <HTMLLabelElement>document.getElementById(output);
-    
+    constructor() {
+        this.markdownChange = new Markdown;
+    }
+    TextChangeHandler(id, output) {
+        let markdown = document.getElementById(id);
+        let markdownOutput = document.getElementById(output);
         if (markdown !== null) {
             markdown.onkeyup = (e) => {
                 this.RenderHtmlContent(markdown, markdownOutput);
-            }
+            };
             window.onload = (e) => {
                 this.RenderHtmlContent(markdown, markdownOutput);
-            }
+            };
         }
     }
-
-    private RenderHtmlContent(markdown: HTMLTextAreaElement, markdownOutput: HTMLLabelElement) {
+    RenderHtmlContent(markdown, markdownOutput) {
         if (markdown.value) {
             markdownOutput.innerHTML = this.markdownChange.ToHtml(markdown.value);
         }
